3c8c225915b96cf4343148aca3a05c057718ce3b
mohanbanoth <mohanbanoth505@gmail.com>
2017-02-15 14:25:01 +0530

    adding raw material

---
+++ Functionsonphonecall.js
@@ -0,0 +1,19 @@
+function retrieveDescription() {
+    var description = Xrm.Page.getAttribute("description").getValue();
+    var subject = Xrm.Page.getAttribute("subject").getValue();
+    if (description !== null && subject !== null) {
+        if (description.indexOf("Recording: http://") !== -1 && subject.indexOf("Mast Client call") !== -1) {
+            Xrm.Page.ui.tabs.get("phonecall").sections.get("phonecall_section_5").setVisible(true);
+        } else {
+            Xrm.Page.ui.tabs.get("phonecall").sections.get("phonecall_section_5").setVisible(false);
+        }
+    }
+    if (Xrm.Page.ui.getFormType() !== 1) {
+        try {
+            parent.$("#WebResource_Playbackwidget")[0].contentDocument.location.reload();
+        }
+        catch (e) {
+            $("#WebResource_Playbackwidget")[0].contentDocument.location.reload();
+        }
+    }
+}
